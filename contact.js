05d42038f22b10ddd7e9009f6c5ead5e581b019a
dipankarghosh2003 <dipankarghosh2003@gmail.com>
2019-10-13 16:03:55 +0530

    1 file modified

--- contact.js
+++ contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import {BrowserRouter as Router,Link} from 'react-router-dom';
+import {BrowserRouter as Router,Link,Switch,Route} from 'react-router-dom';
+import ContactDetails from './ContactDetails';
 class Contact extends React.Component{
   constructor(props){
     super(props);
@@ -29,6 +30,9 @@ class Contact extends React.Component{
       <ul>
       {this.state.contactList.map(contact=><li><Link to={`/contact/${contact.id}`}>{contact.name}</Link></li>)}
       </ul>
+      <Switch>
+        <Route path="/contact/:id" Component={ContactDetails}></Route>
+      </Switch>
       </Router>
       </div>
     )
